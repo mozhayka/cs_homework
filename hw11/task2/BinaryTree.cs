abc9b5a3f77a4e521a22d9c27bffc817de4b4e9a
mozhayka <mozhay2000@gmail.com>
2021-11-22 17:57:50 +0300

    task2

---
+++ hw11/task2/BinaryTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class BinaryTree
+    {
+        public int[] prev { get; private set; }
+        public BinaryTree(int[] prev)
+        {
+            this.prev = prev;
+        }
+    }
+}
---
+++ hw11/task2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace task2
+{
+    class Program
+    {
+        static string Serialize(BinaryTree tree)
+        {
+            string serialized = "";
+            var elem_size = (int)Math.Ceiling(Math.Log2(tree.prev.Length));
+            
+            for (int i = 0; i < elem_size; i++)
+                serialized += '1';
+            serialized += '0';
+
+            foreach (var prev in tree.prev)
+            {
+                int BinaryCode = Convert.ToInt32(Convert.ToString(prev, 2));
+                var code = BinaryCode.ToString($"D{elem_size}");
+                serialized += code;
+            }
+
+            return serialized;
+        }
+
+        static BinaryTree Deserialize(string tree)
+        {
+            int elem_size = 0;
+
+            while (tree[elem_size] == '1')
+                elem_size++;
+
+            int n = (tree.Length - 1 - elem_size) / elem_size;
+            var prev = new int[n];
+            tree = tree.Substring(elem_size + 1);
+
+            for (int i = 0; i < n; i++)
+            {
+                string leaf = tree.Substring(i * elem_size, elem_size);
+                int pre = 0;
+                foreach(var c in leaf)
+                {
+                    pre = pre * 2 + (c - '0');
+                }
+                prev[i] = pre;
+            }
+
+            return new BinaryTree(prev);
+        }
+
+        static void Test(int[] prev)
+        {
+            var tree = new BinaryTree(prev);
+            var ser_tree = Serialize(tree);   
+
+            Console.WriteLine(ser_tree);         
+            var des_tree = Deserialize(ser_tree);
+
+            for (int i = 0; i < prev.Length; i++)
+                Debug.Assert(prev[i] == des_tree.prev[i]);
+        }
+        static void Main(string[] args)
+        {
+            Test(new int[] { 0, 0, 1, 2, 3 });
+        }
+    }
+}
