c6f389b502ff55159a6de8bf00b6dcf81f088e23
mozhayka <mozhay2000@gmail.com>
2021-12-07 23:10:55 +0300

    task2

---
+++ hw13/task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace task2
+{
+    class Program
+    {
+        private static readonly Mutex _mutex = new Mutex(false);
+
+        static void Print10()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                _mutex.WaitOne();
+                Console.WriteLine($"string {i}");
+                Thread.Sleep(100);
+                _mutex.ReleaseMutex();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Print10);
+            Thread thread2 = new Thread(Print10);
+
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+        }
+    }
+}
