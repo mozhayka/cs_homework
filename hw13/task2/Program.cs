2dfcb0cc39271e0a078f369f71144960ffad8c2b
mozhayka <mozhay2000@gmail.com>
2021-12-24 21:25:02 +0300

    fix task2

--- hw13/task2/Program.cs
+++ hw13/task2/Program.cs
@@ -6,22 +6,37 @@ namespace task2
     class Program
     {
         private static readonly Mutex _mutex = new Mutex(false);
-
-        static void Print10()
+        private static bool isFirstWorking = true;
+        static void Print10_1()
         {
             for(int i = 0; i < 10; i++)
             {
+                while(!isFirstWorking)
+                    Thread.Sleep(10);
                 _mutex.WaitOne();
                 Console.WriteLine($"string {i}");
-                Thread.Sleep(100);
+                isFirstWorking = false;
+                _mutex.ReleaseMutex();
+            }
+        }
+
+        static void Print10_2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                while (isFirstWorking)
+                    Thread.Sleep(10);
+                _mutex.WaitOne();
+                Console.WriteLine($"string {i}");
+                isFirstWorking = true;
                 _mutex.ReleaseMutex();
             }
         }
 
         static void Main(string[] args)
         {
-            Thread thread1 = new Thread(Print10);
-            Thread thread2 = new Thread(Print10);
+            Thread thread1 = new Thread(Print10_1);
+            Thread thread2 = new Thread(Print10_2);
 
             thread1.Start();
             thread2.Start();
