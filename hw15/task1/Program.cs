2d526807547405bf45ce9251c6124bb1f9f4cade
mozhayka <mozhay2000@gmail.com>
2021-12-26 23:00:15 +0300

    task1

---
+++ hw15/task1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Jar
+    {
+        readonly int beesCnt;
+        readonly int capacity;
+        int curHoney = 0;
+        Random rand = new Random();
+        CountdownEvent bear;
+
+        public Jar(int N, int X)
+        {
+            beesCnt = N;
+            capacity = X;
+            bear = new CountdownEvent(X);
+        }
+
+        public void Run()
+        {            
+            var bearTask = new Task(() => EatAll());
+            bearTask.Start();
+            var tasks = new Task[beesCnt];
+            for (int i = 0; i < beesCnt; i++)
+            {
+                tasks[i] = new Task(() => AddHoney());
+                tasks[i].Start();
+                for (int j = 0; j < capacity; j++)
+                {
+                    tasks[i].ContinueWith(t => AddHoney()); 
+                }
+            }
+            bearTask.Wait();
+        }
+
+        private void AddHoney()
+        {
+            Thread.Sleep(rand.Next(500, 1000));
+            curHoney++;
+            bear.Signal();
+            Console.WriteLine("+ 1");
+        }
+
+        private void EatAll()
+        {
+            bear.Wait();
+            curHoney = 0;
+            Console.WriteLine($"bear is eating honey");
+            bear.Reset();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jar j = new Jar(3, 10);
+            j.Run();
+        }
+    }
+}
