2cdd529f1317239652aaabaf223922750d0f7ba6
mozhayka <mozhay2000@gmail.com>
2021-12-26 11:12:13 +0300

    task3_1

---
+++ hw14/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace task2
+{
+    public class Foo
+    {
+        public void first() { Console.WriteLine("first"); }
+        public void second() { Console.WriteLine("second"); }
+        public void third() { Console.WriteLine("third"); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new Foo();
+            var t1 = new Thread(f.first);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ hw14/task3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace task3
+{
+    class Program
+    {
+        static void printStr(string s)
+        {
+            Thread.Sleep(100 * s.Length);
+            Console.WriteLine(s);
+        }
+
+        static void Test(string[] str)
+        {
+            var threads = new Thread[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                int n = i;
+                threads[n] = new Thread(() => printStr(str[n]));
+                threads[n].Start();
+            }
+            foreach (var thr in threads)
+                thr.Join();
+
+        }
+
+        static void Main(string[] args)
+        {
+            Test(new string[] { "as", "bqq", "ab", "Hello World!", "c", "abc"});
+        }
+    }
+}
