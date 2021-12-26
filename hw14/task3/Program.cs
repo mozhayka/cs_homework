44d4eca82aee5d3b72c68160ad6bd8549b270a88
mozhayka <mozhay2000@gmail.com>
2021-12-26 11:16:35 +0300

    task3_2

--- hw14/task3/Program.cs
+++ hw14/task3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace task3
@@ -25,9 +26,35 @@ namespace task3
 
         }
 
+        static List<string> sorted = new List<string>();
+        static void printStr1(string s)
+        {
+            Thread.Sleep(100 * s.Length);
+            lock(sorted)
+            {
+                sorted.Add(s);
+            }
+        }
+
+        static void Test2(string[] str)
+        {
+            var threads = new Thread[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                int n = i;
+                threads[n] = new Thread(() => printStr1(str[n]));
+                threads[n].Start();
+            }
+            foreach (var thr in threads)
+                thr.Join();
+            foreach (var w in sorted)
+                Console.Write($"{w} ");
+        }
+
         static void Main(string[] args)
         {
-            Test(new string[] { "as", "bqq", "ab", "Hello World!", "c", "abc"});
+            Test(new string[] { "as", "bqq", "ab", "Hello World!", "c", "abc" });
+            Test2(new string[] { "as", "bqq", "ab", "Hello World!", "c", "abc" });
         }
     }
 }
