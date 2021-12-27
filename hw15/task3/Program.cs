9b41741e43ed1f91465dc15930850aff1b2c247a
mozhayka <mozhay2000@gmail.com>
2021-12-27 13:11:41 +0300

    task3

--- hw15/task3/Program.cs
+++ hw15/task3/Program.cs
@@ -1,34 +1,90 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace task3
 {
-    class Barbershop
+    class Barbershop : IDisposable
     {
         readonly int N;
         int curCnt = 0;
+        Task task;
+        AutoResetEvent _notify = new AutoResetEvent(false);
+        Mutex m = new Mutex();
+        Mutex barber = new Mutex();
+        Random rand = new Random();
+
         public Barbershop(int N)
         {
             this.N = N;
-        }
-
-        public void Run()
-        {
-
+            task = new Task(() => Sleep());
+            task.Start();
         }
 
         public bool Add()
         {
+            m.WaitOne();
+            Console.WriteLine("Try to add");
             if (curCnt >= N)
+            {
+                Console.WriteLine("Queue is full");
                 return false;
+            }
             curCnt++;
+            task.ContinueWith(t => Haircut());
+            Console.WriteLine("Succesfully added");
 
+            _notify.Set();
+            m.ReleaseMutex();
+            return true;
+        }
+
+        private void Haircut()
+        {
+            barber.WaitOne();
+            Console.WriteLine("Haircutting");
+            Thread.Sleep(rand.Next(500, 1000));
+            curCnt--;
+            if(curCnt == 0)
+            {
+                _notify.Reset();
+                task.ContinueWith(t => Sleep());
+            }
+            Console.WriteLine("Haircut ended");
+            barber.ReleaseMutex();
+        }
+
+        private void Sleep()
+        {
+            barber.WaitOne();
+            Console.WriteLine("Sleep");
+            _notify.WaitOne();
+            Console.WriteLine("Awake");
+            barber.ReleaseMutex();
+        }
+
+        public void Dispose()
+        {
+            _notify.Dispose();
+            m.Dispose();
+            barber.Dispose();
         }
     }
     class Program
     {
+        static void Test()
+        {
+            var b = new Barbershop(3);
+            b.Add();
+            b.Add();
+            b.Add();
+            b.Add();
+            Thread.Sleep(5000);
+            b.Add();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Test();
         }
     }
 }
